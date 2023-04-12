3be8bbf3bc273f7d95e9ade62707feba2a80d46e
nilgunnb <nilgunnb@code.edu.az>
2023-04-13 01:52:37 +0400

    uploaded

---
+++ BookApp.app/Services/BookServices.cs
@@ -0,0 +1,57 @@
+﻿
+
+using BookLibrary.core.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BookApp.app.Services
+{
+    internal class BookServices
+    {
+        private Book[] Books = new Book[0];
+
+        public void ShowBooks()
+        {
+            if (Books.Length == 0)
+            {
+                Console.WriteLine("Not available Books");
+            }
+            foreach (var book in Books)
+            {
+                book.GetFullInfo();
+            }
+        }
+
+        public void CreateBook()
+        {
+            Book NewBook = new Book();
+
+            Console.WriteLine("Please, add Name");
+
+
+            NewBook.Name = Console.ReadLine();
+
+           
+
+            while (string.IsNullOrWhiteSpace(NewBook.Name))
+            {
+                Console.WriteLine("Please, add valid name");
+
+                NewBook.Name = Console.ReadLine();
+
+            }
+
+         
+
+
+            Console.WriteLine("Please, add Price");
+
+            NewBook.Price = Convert.ToDouble(Console.ReadLine());
+
+
+            Array.Resize(ref Books, Books.Length + 1);
+
+            Books[Books.Length - 1] = NewBook;
+
+        }
+    }
+}
